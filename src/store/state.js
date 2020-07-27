dd008f5817d074f8f0f0af9bb73a422500b4427f
Bolo030 <846720961@qq.com>
2020-07-27 17:07:07 +0800

    zero

---
+++ src/store/state.js
@@ -0,0 +1,10 @@
+export default{
+    title : "购物车",
+    people:[
+        { name: '张三', age: 40 },
+        { name: '李四', age: 50 },
+        { name: '王五', age: 30 },
+        { name: '赵六', age: 80 },
+        { name: '田七', age: 10 }
+    ]
+}   
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    {{title}}
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  computed:{
+    title(){
+        return this.$store.state.title
+    }
+  },
+  methods:{
+      change(){
+        this.$store.commit('getParam','hello')
+      }
+  }
+}
+</script>
